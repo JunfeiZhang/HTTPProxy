690e5c378b3c990e361aae7475224ccb79031a8a
Junfei Zhang <zhangjunfly@gmail.com>
2018-02-26 14:46:52 -0800

    modify connect header

--- project3.py
+++ project3.py
@@ -4,6 +4,10 @@ import sys
 
 #class client_handler(threading.Thread):
 
+
+def connect(forward_port, forward_host, forward_request):
+	
+
 if __name__ == '__main__':
 	tcp_port = sys.argv[1]
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,17 +17,30 @@ if __name__ == '__main__':
 	client_conn, client_addr = s.accept()
 	# recevie the tcp payload
 	data = client_conn.recv(8192)
+	strings = bytes(data)
 	# split the payload to HTTP initial line and header
-	request, header = data.split('\r\n', 1)
+	request, headers = strings.split('\r\n', 1)
 	# modify the HTTP version
 	list_request = list(request)
 	list_request[len(list_request) - 1] = '0'
-	forward_request = "".join(list_request)
+	forward_request = "".join(list_request).join('\r\n')
+	forward_port = 80
+	forward_host = ""
+	list_headers = headers.split('\r\n')
+	for header in list_headers:
+		if header.startswith('Host:'):
+			host_port = header.split(':')
+			if(len(host_port) == 3):
+				forward_port = host_port[2]
+			forward_host = host_port[1]
+			forward_request += header + '\r\n'
+		elif header.startswith('Connection:'):
+			connection = header
+			forward_request += 'Connection: close\r\n' 
+		else:
+			forward_request += header + '\r\n'
+	print forward_request
 
-	list_headers = header.split('\r\n')
-	host_port = list_headers[0]
+	connect(forward_port, forward_host, forward_request)
 	
-	
-
-
-
+	s.close()
