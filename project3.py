8709d4eeb39e5fc4c176050b59bf6308fd47f5b4
Ma Chen <machen@D-173-250-200-145.dhcp4.washington.edu>
2018-02-27 17:04:08 -0800

    mei you ren he jin zhan

--- project3.py
+++ project3.py
@@ -5,8 +5,34 @@ import sys
 #class client_handler(threading.Thread):
 
 
-def connect(forward_port, forward_host, forward_request):
+def connect_to_server(forward_port, forward_host):
+	sock = socket.socket(socket.AF_INET, # Internet
+	                    socket.SOCK_STREAM) # TCP
 	
+	forward_ip = socket.gethostbyname(forward_host.strip())
+	print forward_host.strip()
+	print forward_ip
+	print forward_port
+	sock.connect((forward_host.strip(), forward_port))
+	#sock.send(forward_request)
+	result = sock.recv(8192)
+	strings = bytes(result)
+	print strings
+	# status, headers = strings.split('\r\n', 1)
+	# print status
+
+def nonconnect_to_server(forward_port, forward_host, forward_request):
+	sock = socket.socket(socket.AF_INET, # Internet
+	                    socket.SOCK_STREAM) # TCP
+	
+	forward_ip = socket.gethostbyname(forward_host.strip())
+	print forward_host.strip()
+	print forward_ip
+	print forward_port
+	sock.connect((forward_host.strip(), forward_port))
+	sock.send(forward_request)
+	result = sock.recv(8192)
+	strings = bytes(result)
 
 if __name__ == '__main__':
 	tcp_port = sys.argv[1]
@@ -17,30 +43,34 @@ if __name__ == '__main__':
 	client_conn, client_addr = s.accept()
 	# recevie the tcp payload
 	data = client_conn.recv(8192)
+
 	strings = bytes(data)
+	
+	http_version_idx = strings.find('HTTP/1.1')
+	connection_idx = strings.find('keep-alive')
+	print 'conn idx', connection_idx
+	forward_request = strings[0:http_version_idx + 7] + '0' + strings[http_version_idx + 8:connection_idx] + 'close' + strings[connection_idx + 10:]
+	print strings
+	print '----------------------------------'
 	# split the payload to HTTP initial line and header
 	request, headers = strings.split('\r\n', 1)
 	# modify the HTTP version
-	list_request = list(request)
-	list_request[len(list_request) - 1] = '0'
-	forward_request = "".join(list_request).join('\r\n')
 	forward_port = 80
 	forward_host = ""
 	list_headers = headers.split('\r\n')
 	for header in list_headers:
-		if header.startswith('Host:'):
+		if header.lower().startswith('host:'):
 			host_port = header.split(':')
 			if(len(host_port) == 3):
-				forward_port = host_port[2]
+				forward_port = int(host_port[2])
 			forward_host = host_port[1]
-			forward_request += header + '\r\n'
-		elif header.startswith('Connection:'):
-			connection = header
-			forward_request += 'Connection: close\r\n' 
-		else:
-			forward_request += header + '\r\n'
 	print forward_request
+	print '-------------------------'
 
-	connect(forward_port, forward_host, forward_request)
+	request_word = request.split(' ')[0]
+	if request_word == 'CONNECT':
+		connect_to_server(forward_port, forward_host)
+	else:
+		nonconnect_to_server(forward_port, forward_host, forward_request)
 	
 	s.close()
